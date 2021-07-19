a62e6d5542e9d2ef8cdd9c4e493b131a9296c7d6
rodrigoTerenci <rodrigoterenci@gmail.com>
2021-07-19 15:31:26 +0000

    para

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 
 var elemento = document.getElementById('dadosPessoais')
 elemento.hidden=true
+
+let change= document.getElementsByClassName('load')
+
+let changeClass= ()=>{
+    
+    change.hidden;
+}
+change.onclick = () => changeClass()
